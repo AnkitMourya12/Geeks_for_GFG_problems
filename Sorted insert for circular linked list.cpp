62531f485237465d47bc08bbdbeaf1005fcdb24e
ANKIT MOURYA <121748364+AnkitMourya12@users.noreply.github.com>
2024-02-05 23:09:51 +0530

    Create Sorted insert for circular linked list.cpp

---
+++ Sorted insert for circular linked list.cpp
@@ -0,0 +1,19 @@
+ Node *sortedInsert(Node* head, int data)
+    {
+       //Your code here 
+       Node *ans = new Node(data);
+       if(!head){
+           ans->next=ans;
+           return ans;
+       }
+       Node *temp=head,*temp2=NULL;
+       do{
+           temp2=temp;
+           temp=temp->next;
+           if(data>=temp2->data and data<=temp->data)break;
+       }while(temp!=head);
+       temp2->next=ans;
+       ans->next=temp;
+       if(data<head->data)return ans;
+       return head;
+    }
