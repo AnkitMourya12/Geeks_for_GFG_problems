bfc54b3b196fd195f0ab83ec4fd15f708c5f870f
ANKIT MOURYA <121748364+AnkitMourya12@users.noreply.github.com>
2024-03-31 12:13:33 +0530

    Create Closest Neighbour in BST.cpp

---
+++ Closest Neighbour in BST.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+ 
+ void solve(Node* root,int n,int &ans){
+      
+      if(root==NULL)
+      return;
+      
+      if(root->key<=n && root->key>ans)
+      ans=root->key;
+      
+      if(root->key<n)
+      solve(root->right,n,ans);
+      else
+      solve(root->left,n,ans);
+  }
+    int findMaxForN(Node* root, int n) {
+        int ans=-1;
+        solve(root,n,ans);
+        return ans;
+    }
+};
