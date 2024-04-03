c2d683bc8c2a2cdf62fd9604126f990bc4930b10
ANKIT MOURYA <121748364+AnkitMourya12@users.noreply.github.com>
2024-04-03 21:45:57 +0530

    Create Kth common ancestor in BST.cpp

---
+++ Kth common ancestor in BST.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+    public:
+    
+    /*You are required to complete below function */
+    int kthCommonAncestor(Node *root, int k,int x, int y)
+    {
+        // your code goes here
+          int total = 1;
+        Node* temp = root;
+        while(root){
+            int data = root->data;
+            if(x<data && y < data){
+                root = root->left;
+                total++;
+            }
+            else if(x>data && y>data){
+                root = root->right;
+                total++;
+            }
+            else break;
+        }
+        total-=k;
+        int cnt = 1;
+        total++;
+        root = temp;
+        while(root){
+            if(total == cnt ) return root->data;
+            int data = root->data;
+            if(x<data && y < data){
+                root = root->left;
+                cnt++;
+            }
+            else if(x>data && y>data){
+                root = root->right;
+                cnt++;
+            }
+            else break;
+        }
+        return -1;
+    }
+};
